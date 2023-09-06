7aec952e57d9f54ef8fbf692010a285edf4cac0c
Ismail-Saleh <57291326+ismail-sl487@users.noreply.github.com>
2023-09-06 09:33:02 +0700

    memperbaiki widget text

--- lib/shared/text_widget.dart
+++ lib/shared/text_widget.dart
@@ -1,44 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:mceasy_widget/shared/theme.dart';
 import 'package:mceasy_widget/theme.dart';
 
+// class METext extends StatelessWidget {
+//   const METext(
+//       {Key? key,
+//       this.fontWeight = FontWeight.w400,
+//       this.color = Colors.black,
+//       this.fontSize = 12,
+//       this.letterSpacing = 0.5,
+//       this.height = 1,
+//       this.overflow,
+//       this.maxLine,
+//       required this.text,
+//       this.textAlign,})
+//       : super(key: key);
+
+//   final String text;
+//   final FontWeight fontWeight;
+//   final Color color;
+//   final double fontSize, letterSpacing, height;
+//   final TextOverflow? overflow;
+//   final int? maxLine;
+//   final TextAlign? textAlign;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       text,
+//       style: TextStyle(
+//           color: color,
+//           fontSize: fontSize,
+//           letterSpacing: letterSpacing,
+//           height: height,
+//           overflow: overflow,
+//           fontFamily: setFontSize(fontWeight)),
+//       maxLines: maxLine,
+//       textAlign: textAlign,
+//     );
+//   }
+// }
+
 class METext extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color? color;
+  final int? maxLines;
+  final double? lineHeight;
+  final TextAlign? textAlign;
+  final double? height;
+  final double? letterSpacing;
   const METext(
       {Key? key,
-      this.fontWeight = FontWeight.w400,
-      this.color = Colors.black,
-      this.fontSize = 12,
-      this.letterSpacing = 0.5,
-      this.height = 1,
-      this.overflow,
-      this.maxLine,
       required this.text,
-      this.textAlign,})
+      this.fontSize = 12,
+      this.textAlign,
+      this.fontWeight = FontWeight.w400,
+      this.lineHeight,
+      this.height = 1,
+      this.color,
+      this.maxLines,
+      this.letterSpacing = 0.2})
       : super(key: key);
-
-  final String text;
-  final FontWeight fontWeight;
-  final Color color;
-  final double fontSize, letterSpacing, height;
-  final TextOverflow? overflow;
-  final int? maxLine;
-  final TextAlign? textAlign;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-          color: color,
-          fontSize: fontSize,
-          letterSpacing: letterSpacing,
-          height: height,
-          overflow: overflow,
-          fontFamily: setFontSize(fontWeight)),
-      maxLines: maxLine,
-      textAlign: textAlign,
+    return RichText(
+      maxLines: maxLines,
+      textAlign: textAlign ?? TextAlign.left,
+      text: TextSpan(
+          text: text,
+          style: CommonTextStyle.style(
+              letterSpacing: letterSpacing,
+              heightSpacingText: height,
+              lineHeight: lineHeight,
+              color: color ?? Pallets.navy80,
+              fontWeight: fontWeight ?? FontWeight.w400,
+              fontSize: fontSize ?? 14)),
     );
   }
 }
+
 
 class CommonTextStyle {
   static TextStyle style({
