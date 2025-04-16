0ab4c915ec1b9202b15cd4f6a2f2704226ab3ffd
Era Prima S <eraprimax@gmail.com>
2025-04-16 14:44:04 +0700

    Update textfield_widget.dart

--- lib/shared/textfield_widget.dart
+++ lib/shared/textfield_widget.dart
@@ -65,6 +65,10 @@ class METextfield extends StatelessWidget {
             borderRadius: BorderRadius.circular(borderRadius!),
             borderSide: BorderSide(color: secondaryColor ?? const Color(0xFF99A1B1), width: 1),
           ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borderRadius!),
+            borderSide: BorderSide(color: secondaryColor ?? const Color(0xFF99A1B1), width: 1),
+          ),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(borderRadius!),
           ),
